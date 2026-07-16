915a76ab163b12620c01364e13a48eb87818d135
unknown <helinazeleke670@gmail.com>
2026-07-16 21:41:20 +0300

    chore: save latest changes

--- server/utils/env.js
+++ server/utils/env.js
@@ -3,10 +3,10 @@ dotenv.config();
 
 const env = {
   port:          process.env.PORT || 6000,
-  mongoUri:      process.env.MONGODB_URI,
+  mongoUri:      process.env.DATABASE_URL,
   jwtSecret:     process.env.JWT_SECRET,
   jwtExpire:     process.env.JWT_EXPIRE || '30d',
-  emailUser:     process.env.EMAIL_USER,
+  emailUser:     process.env.EMAIL,
   emailPassword: process.env.EMAIL_PASSWORD,
   frontendUrl:   process.env.FRONTEND_URL || 'http://localhost:3000',
 };
